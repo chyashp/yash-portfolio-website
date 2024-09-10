0bf9e5c03a38fba58b258a85e9afba14d8c7669c
Yash <hasypreet23@gmail.com>
2024-09-10 17:39:04 -0400

    removed the name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,13 +7,7 @@ function navbar() {
     <div className="navbar">
       <Sidebar />
       <div className="wrapper">
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          Lama Dev
-        </motion.span>
+
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
